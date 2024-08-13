75829e983fac56afef6755740b3c0584eb8c27ca
Alex Barker <alex.barker234@gmail.com>
2024-08-13 12:09:52 +0800

    railway volume

--- rawpad/server.js
+++ rawpad/server.js
@@ -8,7 +8,9 @@ const app = express();
 app.use(express.json());
 const port = process.env.PORT ?? 80;
 
-const filePath = path.join(__dirname, "data.txt");
+const dataDir = process.env.RAILWAY_VOLUME_MOUNT_PATH ?? ".";
+
+const filePath = path.join(dataDir, "data.txt");
 const index = path.join(__dirname, "index.html");
 
 var lastCopy = new Date();
@@ -59,14 +61,14 @@ app.get("/", (req, res) => {
 
 app.post("/save", (req, res) => {
     try {
-      if (!req.headers.authorization?.includes(process.env.PASS)) return res.status(401);
+        if (!req.headers.authorization?.includes(process.env.PASS)) return res.status(401);
 
-      const newContent = req.body.content;
+        const newContent = req.body.content;
 
         fs.writeFileSync(filePath, newContent, "utf8");
         content = newContent;
         lastCopy = new Date();
-        res.send("Saved successfully!");
+        res.status(200).send("Saved successfully!");
     } catch (err) {
         console.error("Error saving file:", err);
         res.status(500).send("Error saving data!");
